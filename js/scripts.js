91e91755d357df8a94052cb3dc6f82c9908c3356
Basma Siam <basma.siam@gmail.com>
2022-11-02 12:24:59 -0700

    Adding new website using javascript

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+window.onload=function(){
+const form=document.querySelector("form");
+form.onsubmit=function(event){
+  event.preventDefault();
+  let votingLinks=document.getElementById("above-18");
+  votingLinks.setAttribute("class","hidden");
+  let votingInfo=document.getElementById("below-18");
+  votingInfo.setAttribute("class","hidden");
+
+  let age=parseInt(document.querySelector("input#age").value)
+if (age>17) {
+
+votingLinks.removeAttribute("class");
+
+} else {
+votingInfo.removeAttribute("class");
+}
+};
+};
